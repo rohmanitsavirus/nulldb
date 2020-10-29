26dc4c148f0d03b360e4858b7a00fe3f287d2e3a
yaojie <limyaojie93@gmail.com>
2020-10-29 16:02:50 +0800

    use string instead of id

--- lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
+++ lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
@@ -2,7 +2,7 @@ class ActiveRecord::ConnectionAdapters::NullDBAdapter
 
   class TableDefinition < ActiveRecord::ConnectionAdapters::TableDefinition
     attr_accessor :name
-    alias_method :uuid, :id
+    alias_method :uuid, :string
     alias_method :citext, :text
     alias_method :interval, :text
     alias_method :geometry, :text
