1db1ff143f8c05e8aee80e02e8597b8f97ae96f1
tsmartt <81187695+tsmartt@users.noreply.github.com>
2021-07-28 13:21:46 -0700

    Add inet column support

--- lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
+++ lib/active_record/connection_adapters/nulldb_adapter/table_definition.rb
@@ -7,6 +7,7 @@ class ActiveRecord::ConnectionAdapters::NullDBAdapter
     alias_method :interval, :text
     alias_method :geometry, :text
     alias_method :serial, :integer
+    alias_method :inet, :string
     alias_method :jsonb, :json if method_defined? :json
   end
 end
